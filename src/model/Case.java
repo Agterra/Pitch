eaafcc87a07b0a01eeccb59e34df04f829426c71
Louis <louis.lebrun75@gmail.com>
2018-03-28 10:59:17 +0200

    Added Models
    - Added Lien
    - Added Symbole

--- src/model/Case.java
+++ src/model/Case.java
@@ -11,4 +11,56 @@ package model;
  */
 public class Case {
     
+    private Symbole symbole;
+    
+    private Lien lien;
+    
+    private int x;
+    
+    private int y;
+    
+    public Case () {
+        
+        this.x = -1;
+        
+        this.y = -1;
+        
+        this.symbole = Symbole.VIDE;
+        
+        this.lien = Lien.VIDE;
+        
+    }
+
+    public Symbole getSymbole() {
+        return symbole;
+    }
+
+    public void setSymbole(Symbole symbole) {
+        this.symbole = symbole;
+    }
+
+    public Lien getLien() {
+        return lien;
+    }
+
+    public void setLien(Lien lien) {
+        this.lien = lien;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
 }
