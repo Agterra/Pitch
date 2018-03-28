aa8cc39091812f4dd1b37470c4602f6505245674
Louis <louis.lebrun75@gmail.com>
2018-03-28 12:04:13 +0200

    Added main application view and main

--- src/model/Grille.java
+++ src/model/Grille.java
@@ -127,7 +127,7 @@ public class Grille extends Observable{
             
         this.setChanged();
         
-        this.notifyAllObservers();
+        this.notifyObservers();
         
     }
     
---
+++ src/vue/MainApplication.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vue;
+
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+
+/**
+ *
+ * @author agterra
+ */
+public class MainApplication extends Application{
+
+    public static void main(String[] args) {
+        
+        launch(args);
+        
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("COUCOU, JE SUIS UN BORDEUR LA YURT");
+        
+    }
+    
+}
